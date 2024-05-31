d5c0c8907ef6c6ad88dc4cc5d221855ac45a09a6
Zhansaya <63559079+soulzest@users.noreply.github.com>
2024-05-30 23:45:55 -0700

    add one java challenge

---
+++ src/test/java/JavaProgrammingPractice/FindMax.java
@@ -0,0 +1,25 @@
+package JavaProgrammingPractice;
+
+public class FindMax {
+    /*
+        Write a function that can find the maximum number from an int Array
+     */
+
+
+        public int maxValue(int[] n) {
+
+            int max = Integer.MIN_VALUE;
+
+            for (int each : n) {
+                if (each > max) {
+                    max = each;
+                }
+            }
+
+            return max;
+        }
+
+
+    }
+
+
